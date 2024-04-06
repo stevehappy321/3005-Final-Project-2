58a9689e6f4699419fb2d33cc0156be2da9b875a
stevehappy321 <90816857+stevehappy321@users.noreply.github.com>
2024-04-06 18:47:33 -0400

    function fixes (#3)
    * Function renaming
    
    * function fixes
    
    * Create .gitignore
    
    * Delete main.py
    
    ---------
    
    Co-authored-by: Ryaneroni <132392500+Ryaneroni@users.noreply.github.com>

--- MemberBackend.py
+++ MemberBackend.py
@@ -8,12 +8,30 @@ def getAvailableTrainers(date, startTime, endTime):
     #get all trainers
     #remove trainers that are occupied during the specified timeframe
 
-    allTrainersID = SQL.StrictSelect("SELECT Trainers.trainerID, Trainers.firstName, Trainers.lastName FROM Trainers") #list of tuples
+    allTrainers = SQL.StrictSelect("SELECT Trainers.trainerID, Trainers.firstName, Trainers.lastName FROM Trainers") #list of tuples
     availableTrainers = []
 
-    for trainer in allTrainersID:
+    for trainer in allTrainers:
         trainerID = trainer[0]
         if TrainerBackend.trainerIsAvailable(trainerID, date, startTime, endTime):
             availableTrainers.append(trainer)
 
-    return availableTrainers
+    return availableTrainers; #list of tuples
+
+"""
+def getAvailableTrainers_v2(date, startTime, endTime):
+    #same as getAvailableTrainers, but returns a list of dictionaries instead of a list of tuples
+
+    allTrainers = SQL.StrictSelect("SELECT Trainers.trainerID, Trainers.firstName, Trainers.lastName FROM Trainers") #list of tuples
+
+    for e in allTrainers: #convert list of tuples to list of struct-type dictionaries
+        e = Utility.tupleToDict(tuple, ["trainerID, firstName, lastName"])
+        
+    availableTrainers = []
+
+    for trainer in allTrainers:
+        if TrainerBackend.trainerIsAvailable(trainer["trainerID"], date, startTime, endTime):
+            availableTrainers.append(trainer)
+
+    return availableTrainers; #list of "structs"
+"""
--- TrainerBackend.py
+++ TrainerBackend.py
@@ -35,7 +35,7 @@ def getTrainerHours(trainerID):
     return Utility.tupleToDict( trainerHours, ["startTime", "endTime"] )
 
 def trainerIsAvailable(trainerID, date, startTime, endTime):
-    trainerFreeIntervals = getTrainerAvailability(date, trainerID)
+    trainerFreeIntervals = getTrainerAvailableInvervals(date, trainerID)
 
     for interval in trainerFreeIntervals: #interval is a tuple (startTime, endTime)
         if not(
@@ -46,43 +46,42 @@ def trainerIsAvailable(trainerID, date, startTime, endTime):
         
     return True
 
-def getTrainerAvailability(date, trainerID):
+def getTrainerAvailableInvervals(date, trainerID):
+    def computeFreeIntervals(startTime, endTime, busyIntervals):
+        freeIntervals = []
+
+        if len(busyIntervals) == 0:
+            freeIntervals.append( (startTime, endTime) )
+        
+        else:
+            for i in range(0, len(busyIntervals) ):
+                if i == 0:
+                    freeIntervals.append( (startTime, busyIntervals[i]["startTime"]) )
+                else:
+                    freeIntervals.append( (busyIntervals[i-1]["endTime"], busyIntervals[i]["startTime"]) )
+
+            if busyIntervals[-1]["endTime"] != endTime:
+                freeIntervals.append( (busyIntervals[-1]["endTime"], endTime) )
+
+        return freeIntervals;
+
+
     startTime = getTrainerHours(trainerID)["startTime"]
     endTime = getTrainerHours(trainerID)["endTime"]
 
-    busyIntervalsRecords = SQL.StrictSelect( #list of date-time pairs where this trainer is busy
+    busyIntervals = SQL.StrictSelect( #list of tuples where this trainer is busy
         f"""
-        SELECT classDate AS date, sessionTime AS startTime, endTime FROM FitnessClass
+        SELECT sessionTime AS startTime, endTime FROM FitnessClass
         WHERE trainerID = {trainerID} AND classDate = '{date}'
         UNION
-        SELECT sessionDate AS date, sessionTime AS startTime, endTime FROM PrivateSession
+        SELECT sessionTime AS startTime, endTime FROM PrivateSession
         WHERE trainerID = {trainerID} AND sessionDate = '{date}'
         """)
-
-    busyIntervals = []
-    for occupiedInterval in busyIntervalsRecords:
-        busyIntervals.append( Utility.tupleToDict(occupiedInterval, ["date", "startTime", "endTime"]) )
-
-    return computeAvailability(startTime, endTime, busyIntervals); #returns a list of tuple intervals that this trainer is free to teach
-
-
-def computeAvailability(startTime, endTime, busyIntervals):
-    freeIntervals = []
-
-    if len(busyIntervals) == 0:
-        freeIntervals.append( (startTime, endTime) )
     
-    else:
-        for i in range( 0, len(busyIntervals) ):
-            if i == 0:
-                freeIntervals.append( (startTime, busyIntervals[i]["startTime"]) )
-            else:
-                freeIntervals.append( (busyIntervals[i-1]["endTime"], busyIntervals[i]["startTime"]) )
-        
-        if busyIntervals[len(busyIntervals)-1]["endTime"] != endTime:
-            freeIntervals.append( (busyIntervals[len(busyIntervals)-1]["endTime"], endTime) )
+    for i in range (0, len(busyIntervals) ):
+        busyIntervals[i] = Utility.tupleToDict(busyIntervals[i], ["startTime", "endTime"])
 
-    return freeIntervals;
+    return computeFreeIntervals(startTime, endTime, busyIntervals); #returns a list of tuple intervals that this trainer is free to teach
 
 
 def searchMemberByName(name):
