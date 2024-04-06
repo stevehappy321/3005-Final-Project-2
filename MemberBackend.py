02229a8be90764e12773fad00375370403680bfd
stevehappy321 <90816857+stevehappy321@users.noreply.github.com>
2024-04-06 17:22:56 -0400

    Update MemberBackend.py

--- MemberBackend.py
+++ MemberBackend.py
@@ -9,9 +9,11 @@ def getAvailableTrainers(date, startTime, endTime):
     #remove trainers that are occupied during the specified timeframe
 
     allTrainersID = SQL.StrictSelect("SELECT Trainers.trainerID, Trainers.firstName, Trainers.lastName FROM Trainers") #list of tuples
-    availableTrianers = []
+    availableTrainers = []
 
     for trainer in allTrainersID:
         trainerID = trainer[0]
         if TrainerBackend.trainerIsAvailable(trainerID, date, startTime, endTime):
-            availableTrianers.append(trainer)
+            availableTrainers.append(trainer)
+
+    return availableTrainers
