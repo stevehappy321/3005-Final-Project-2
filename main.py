9341f6489e06332becf8e2177d7993d82dab674a
stevehappy321 <90816857+stevehappy321@users.noreply.github.com>
2024-04-09 16:12:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,60 +1,41 @@
-import AdminGUI, MemberGUI
+import AdminGUI, MemberGUI, TrainerGUI
 import InitializeGUI
 import SQL
-
-import TrainerGUI
-import TrainerBackend
-#from  TrainerPages import TrainerMenu #alternative to TrainerGUI
-
-import datetime
 
 #Ryan
 
 user_input_global = None
-"""
-def user_input_received(user_input):    
+
+def user_input_received(user_input):
     global user_input_global
     user_input_global = user_input
-    if(user_input_global == 'ADMIN'):
-        InitializeGUI.broadcast(True, "Now logged in as: {}".format(user_input_global))
+    user_input_global = user_input_global.split(":")
+    if(user_input_global[0] == 'Admin' and user_input_global[-2] == 'ADMIN'):
+        InitializeGUI.broadcast(True, "Now logged in as: {}".format(user_input_global[-2]))
         InitializeGUI.close_gui()
         AdminGUI.AdminPortal()
-    else:
-        if(SQL.personExists2('Trainers', user_input_global) == True):
-            allNames = user_input_global.split(' ')
-
-            firstName = allNames[0]
-            lastName = allNames[1] if len(allNames) >= 2 else ''
-            print(firstName, lastName)
-
-            trainerID = SQL.StrictSelect(
-                "SELECT trainerID "
-                "FROM Trainers "
-                f"WHERE firstname = '{firstName}' AND lastname = '{lastName}'")[0][0]
-            
-            print('trainer id query = ')
-            print(trainerID)
-            
-            #InitializeGUI.broadcast(True, "User Found! Now logged in as: {}".format(user_input_global))
+    elif (user_input_global[0] == 'Member'):
+        if(SQL.personExists(user_input_global[-2]) == True):
+            InitializeGUI.broadcast(True, "User Found! Now logged in as: {}".format(user_input_global[-2]))
             InitializeGUI.close_gui()
-            #TrainerGUI.trainerPortal(trainerID);
-            TrainerMenu.trainerPortal(trainerID)
+            MemberGUI.MemberPortal(user_input_global[-2])
         else:
-            InitializeGUI.broadcast(False, "No User Found")
-    """
-
-print( TrainerBackend.getTrainerAvailability(datetime.date(2024, 4, 15) , 2) )
-TrainerGUI.trainerPortal(2)
-#AdminGUI.AdminPortal()
-
-
+            InitializeGUI.broadcast(False, "No Member Found")
+    elif (user_input_global[0] == 'Trainer'):
+        if (SQL.personExists2("Trainers", user_input_global[-2]) == True):
+            InitializeGUI.broadcast(True, "User Found! Now logged in as: {}".format(user_input_global[-2]))
+            InitializeGUI.close_gui()
+            #@STEVE CHANGE IN UR TRAINER PORTAL
+            TrainerGUI.trainerPortal()
+        else:
+            InitializeGUI.broadcast(False, "No Trainer Found")
+    else:
+        InitializeGUI.broadcast(False, "Login Not Valid. Try ADMIN")
+    
 
 if __name__ == "__main__":
-    #InitializeGUI.Initialize(user_input_received)
-    #AdminGUI.AdminPortal()
-    #MemberGUI.MemberPortal(user_input_global)
-    #TrainerGUI.TrainerPortal()
+    InitializeGUI.Initialize(user_input_received)
     if user_input_global is not None:
-        print("User input was:", user_input_global)
+        print("User input was:", user_input_global[1])
     else:
         print("No user input was received before the AdminGUI was closed.")
